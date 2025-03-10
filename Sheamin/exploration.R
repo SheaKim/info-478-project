75d7d3a1c0f825f22173ba21a947db17546d8992
SheaKim <114708841+SheaKim@users.noreply.github.com>
2025-03-10 00:37:19 -0700

    more plotting and some tests
    more plotting and some tests

--- Sheamin/exploration.R
+++ Sheamin/exploration.R
@@ -9,7 +9,7 @@ vax_df$CDC.Cost..Dose = gsub("\\$", "", vax_df$CDC.Cost..Dose)
 vax_df$Private.Sector.Cost..Dose <- as.numeric(as.character(vax_df$Private.Sector.Cost..Dose))
 sapply(vax_df, class)
 
-vax_df$vax_df$CDC.Cost..Dose <- as.numeric(as.character(vax_df$CDC.Cost..Dose))
+vax_df$CDC.Cost..Dose <- as.numeric(as.character(vax_df$CDC.Cost..Dose))
 sapply(vax_df, class)
 
 vax_df$Date <- as.Date(vax_df$Date)
@@ -51,7 +51,50 @@ plot_1 <- vax_df %>%
 
 print(plot_1)
 
+private_cdc_comparison_plot <- vax_df %>%
+  group_by(year) %>%
+  summarise(average_priv_price = mean(Private.Sector.Cost..Dose), 
+            average_cdc_price = mean(CDC.Cost..Dose)) %>%
+  pivot_longer(cols = c("average_priv_price", "average_cdc_price"), 
+               names_to = "price_type", 
+               values_to = "price") %>%
+  ggplot(aes(x=factor(year), y=price, group=price_type)) +
+  geom_line(aes(color=price_type))
 
+private_cdc_comparison_plot
+
+# lin reg
+model <- lm(adjusted_price ~ year, data = vax_df)
+print(summary(model))
+
+ggplot(vax_df, aes(x = year, y = adjusted_price)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+#one tailed t test
+
+test <- t.test(vax_df$Private.Sector.Cost..Dose, vax_df$CDC.Cost..Dose,
+               alternative = "greater")
+test
+
+
+# Boxplots of adjusted_price by Manufacturer
+
+vax_df$Manufacturer <- str_replace_all(vax_df$Manufacturer,"CSL",'bioCSL')
+vax_df$Manufacturer <- str_replace_all(vax_df$Manufacturer,"biobioCSL",'bioCSL')
+
+
+boxplot <- ggplot(vax_df, aes(x = Manufacturer, y = Private.Sector.Cost..Dose)) +
+  geom_boxplot() +
+  theme_minimal() +
+  labs(
+    title = "Distribution of Adjusted Prices by Manufacturer",
+    x = "Manufacturer",
+    y = "Private Sector Price"
+  ) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+
+boxplot
 
 
 
